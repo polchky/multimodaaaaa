e69a5ed8d0344c2815545a8ac8ed3712adc9c1d3
Ismaïl Senhaji <ismail.senhaji@gmail.com>
2014-05-11 22:12:11 +0200

    Add functionality to easily emulate keyboard/mouse
    uinput and/or xboxdrv don't want to cooperate, we will use keyboard/
    mouse instead of joystick emulation.

--- joystick.py
+++ joystick.py
@@ -8,9 +8,14 @@ from glove import Glove
 class Joystick:
     ANALOG = 0
     BUTTON = 1
-    MIN_PRESS_TIME = 200 # milliseconds
-    MIN_REPRESS_TIME = 700 # milliseconds
-    DEFAULT_ACTIONS = {
+    FALSE_ANALOG = 2
+    MOUSE = 3
+
+    MIN_PRESS_TIME = 200  # milliseconds
+    MIN_REPRESS_TIME = 700  # milliseconds
+    ANALOG_RANGE = (-0x8000, 0x8000, 0, 0)
+
+    DEFAULT_ACTIONS_JOYSTICK = {
         'X': (uinput.ABS_X, ANALOG),
         'Y': (uinput.ABS_Y, ANALOG),
         'RX': (uinput.ABS_RX, ANALOG),
@@ -21,11 +26,38 @@ class Joystick:
         'yeah': (uinput.BTN_3, BUTTON),
         'walk': (uinput.KEY_W, BUTTON)
     }
+
+    # TODO: Map correct keys
+    DEFAULT_ACTIONS_KEYBOARD = {
+        'X': ((uinput.KEY_W, uinput.KEY_S), FALSE_ANALOG),
+        'Y': ((uinput.KEY_D, uinput.KEY_A), FALSE_ANALOG),
+        'RX': (uinput.REL_X, ANALOG),
+        'RY': (uinput.ABS_Y, ANALOG),
+        'para': (uinput.KEY_SPACE, BUTTON),
+        'graf': (uinput.KEY_LEFTCTRL, BUTTON),
+        'fuck': (uinput.KEY_LEFTALT, BUTTON),
+        'yeah': (uinput.KEY_LEFTSHIFT, BUTTON),
+        'walk': (uinput.KEY_W, BUTTON)
+    }
+
     DEV_INPUT = '/dev/input/'
-    
-    def __init__(self, actions=DEFAULT_ACTIONS, name='Gamepad'):
+
+    def __init__(self, actions=DEFAULT_ACTIONS_KEYBOARD, name='Gamepad'):
         self.actions = actions
-        self.state = dict.fromkeys(actions, 0)
+
+        uinput_events = []
+        for a, t in actions.values():
+            if t in (Joystick.BUTTON, Joystick.MOUSE):
+                uinput_args.append(a)
+            if t == Joystick.ANALOG:
+                uinput_args.append(a + Joystick.ANALOG_RANGE)
+            elif t == Joystick.FALSE_ANALOG:
+                uinput_args.extend(a)
+
+        ev_before = [f for f in os.listdir(Joystick.DEV_INPUT) if f.startswith('event')]
+        self.device = uinput.Device(uinput_events, name)
+        ev_after = [f for f in os.listdir(Joystick.DEV_INPUT) if f.startswith('event')]
+        self.event = Joystick.DEV_INPUT + list(set(ev_after) - set(ev_before))[0]
 
         self.parachute_opening = False
         self.last_gesture = Glove.FINGER_POSITIONS['OPEN']
@@ -33,17 +65,15 @@ class Joystick:
         self.last_time_changed = 0
         self.ready_to_graffiti = False
 
-        ev_before = [f for f in os.listdir(Joystick.DEV_INPUT) if f.startswith('event')]
-        self.device = uinput.Device([a[0] for a in actions.values()], name)
-        ev_after = [f for f in os.listdir(Joystick.DEV_INPUT) if f.startswith('event')]
-        self.event  = Joystick.DEV_INPUT + list(set(ev_after) - set(ev_before))[0]
-        
-    """
-    def update(self, XY, fingers):
-        for k in actions:
-            self.emit(k, actions[k])
-        self.device.syn()
-    """
+    def update(self, direction, fingers):
+        self.update_joystick(direction)
+        self.update_buttons(fingers)
+
+    def update_joystick(self, direction):
+        x, y = direction
+        for k, v in zip(["X", "Y", "RX", "RY"], [x, y, 10*x, 10*y]):
+            self.emit(k, v)
+
     def update_buttons(self, fingers):
         if fingers == self.last_gesture:
             if self.check_last_changed() and self.check_last_pressed():
@@ -54,60 +84,71 @@ class Joystick:
             self.parachute_opening = False
             if fingers != Glove.FINGER_POSITIONS['FIST']:
                 self.ready_to_graffiti = False
-             
+
     def emit_from_fingers(self, fingers):
-        if fingers == Glove.FINGER_POSITIONS['FUCK']:self.emit('fuck', 1)
-        elif fingers == Glove.FINGER_POSITIONS['YEAH']:self.emit('yeah', 1)
-        elif fingers == Glove.FINGER_POSITIONS['GRAF']:self.ready_to_graffiti = True
+        if fingers == Glove.FINGER_POSITIONS['FUCK']:
+            self.emit('fuck', 1)
+        elif fingers == Glove.FINGER_POSITIONS['YEAH']:
+            self.emit('yeah', 1)
+        elif fingers == Glove.FINGER_POSITIONS['GRAF']:
+            self.ready_to_graffiti = True
         elif fingers == Glove.FINGER_POSITIONS['FIST']:
             if self.ready_to_graffiti:
                 self.emit('graf', 1)
                 self.ready_to_graffiti = False
             else:
-                self.parachute_opening = True  
+                self.parachute_opening = True
         self.last_time_pressed = self.get_time()
-            
+
     def open_parachute(self):
         self.emit('para', 1)
-    
-    def update_joystick(self, XY):
-        pass
-    
-    def walk(self,fingers):
+
+    # TODO: Remove "walk" action (here and in DEFAULT_ACTIONS_*),
+    #  replace with "X" as it's supposed to be
+    def walk(self, fingers):
         if self.last_gesture == Glove.FINGER_POSITIONS['OPEN']:
             self.device.emit('walk', 1)
         if fingers == Glove.FINGER_POSITIONS['OPEN']:
             self.device.emit('walk', 0)
             self.last_time_changed = self.get_time()
         self.last_gesture = fingers
-        
+
     def is_walking(self):
         return self.last_gesture != Glove.FINGER_POSITIONS['OPEN']
-        
+
     def reset(self):
         for action in self.actions:
-            self.emit(action, 0)
+            self.emit(action, 0, False)
+        self.device.syn()
 
-    def emit(self, k, v, syn=False):
+    def emit(self, k, v, syn=True):
         if k not in self.actions:
             return
-        if self.actions[k][1] == Joystick.ANALOG:
+        elif self.actions[k][1] in (Joystick.ANALOG, Joystick.MOUSE):
             self.device.emit(self.actions[k][0], v, syn)
         elif self.actions[k][1] == Joystick.BUTTON and v != 0:
             self.device.emit_click(self.actions[k][0], syn)
-            
+        elif self.actions[k][1] == Joystick.FALSE_ANALOG:
+            ks = self.actions[k][0]
+            vs = [v > 0, v < 0]
+            for i in (0, 1):
+                self.device.emit(ks[i], vs[i], syn)
+
     def is_parachute_opening(self):
         return self.parachute_opening
-        
-    def get_time(self):
-        return int(round(time.time() * 1000))
-        
+
     def check_last_pressed(self):
-        if self.last_time_pressed < self.last_time_changed:return True
-        return self.get_time() - self.last_time_pressed > self.MIN_REPRESS_TIME
-        
+        if self.last_time_pressed < self.last_time_changed:
+            return True
+        else:
+            return self.get_time() - self.last_time_pressed > self.MIN_REPRESS_TIME
+
     def check_last_changed(self):
         return self.get_time() - self.last_time_changed > self.MIN_PRESS_TIME
 
     def start_xboxdrv(self):
         subprocess.Popen(['./start_xboxdrv.sh', self.event])
+
+    @staticmethod
+    def get_time():
+        return int(round(time.time() * 1000))
